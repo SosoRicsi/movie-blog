99df271d5cb64eff675677db846505d248fb42df
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-11-04 12:56:03 +0100

    Remove Ziggy's types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,6 +1,5 @@
 import type { PageProps } from '@inertiajs/core';
 import type { LucideIcon } from 'lucide-vue-next';
-import type { Config } from 'ziggy-js';
 
 export interface Auth {
     user: User;
@@ -27,7 +26,6 @@ export interface SharedData extends PageProps {
     name: string;
     quote: { message: string; author: string };
     auth: Auth;
-    ziggy: Config & { location: string };
     sidebarOpen: boolean;
 }
 
