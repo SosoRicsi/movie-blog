b090c42c9ad2a192cf351b26904d45080520e102
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-11-07 02:50:55 +0100

    Fix namespace

--- app/Models/FilmTranslation.php
+++ app/Models/FilmTranslation.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Enums\Enums\FilmLanguages;
+use App\Enums\FilmLanguages;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
